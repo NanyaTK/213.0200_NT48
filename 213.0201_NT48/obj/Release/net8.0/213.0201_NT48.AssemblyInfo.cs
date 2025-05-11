86c4a72a8123204774cccfeeef30015498ff5a78
NanyaTK <herenfuyong02@gmail.com>
2025-05-11 00:10:43 +0000

    v0.0.1-2025051102

--- 213.0201_NT48/obj/Release/net8.0/213.0201_NT48.AssemblyInfo.cs
+++ 213.0201_NT48/obj/Release/net8.0/213.0201_NT48.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("213.0201_NT48")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cece7b6a8525f833f87941058a86909068694fe1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2adb592612c0ab1b61b5fbae746216634331667a")]
 [assembly: System.Reflection.AssemblyProductAttribute("213.0201_NT48")]
 [assembly: System.Reflection.AssemblyTitleAttribute("213.0201_NT48")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
